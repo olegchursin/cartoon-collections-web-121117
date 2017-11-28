f180eb53119e005b335ff0a44d00edda2bef6133
Oleg Chursin <olegchursin@gmail.com>
2017-11-28 17:47:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,9 @@ def summon_captain_planet(array)
   array.map { |e| e.capitalize << "!" }
 end
 
+
 def long_planeteer_calls(array)
-  array.find { |e| e.length > 4 }
+  array.any? { |e| e.length > 4 }
 end
 
 def find_the_cheese# code an argument here
