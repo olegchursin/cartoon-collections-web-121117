24a72f854d0be367d235c3daec24e7f1edbfbfbc
Oleg Chursin <olegchursin@gmail.com>
2017-11-28 17:44:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,17 +1,18 @@
-#This method should accept an array of dwarf names. It should then print out each name using puts.
+#This method should accept an array of dwarf names. It should then print out each name using puts into
+#numbered list
 def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |dwarf, index|
     puts "#{index+1}. #{dwarf}"
   end
 end
 
-
+#capitalize each element and add an exclamation point at the end
 def summon_captain_planet(array)
   array.map { |e| e.capitalize << "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.find { |e| e.length > 4 }
 end
 
 def find_the_cheese# code an argument here
