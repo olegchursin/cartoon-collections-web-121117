00c2e35c6533bd20e8e207627fab2ba05b00ec5e
Oleg Chursin <olegchursin@gmail.com>
2017-11-28 17:35:27 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,9 @@ end
 
 
 def summon_captain_planet(array)
-  array.collect { |e| e.upcase << "!"  }
+  array.collect do |e|
+    e.upcase << "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
