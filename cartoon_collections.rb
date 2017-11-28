4e101eecef17c2bf1aa4514e01f3d99b77e29503
Oleg Chursin <olegchursin@gmail.com>
2017-11-28 18:33:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,8 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.any? do |e|
-    cheese_types.include?(e)
+  array.any? { |e| cheese_types.include?(e) }
+  if array.any? == true
     puts e
   end
 end
